e1909865d992f5df5a34f2b8b295aaa1af020285
Juan Lee <juanlee@kaist.ac.kr>
2019-05-27 18:55:14 +0900

    Create except search

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import Credentials as C
 import pymysql
+import random
 
 #from Credentials import host, port, user, passwd, db, charset;
 
@@ -32,26 +33,101 @@ def signIn(id, pw, type):
 
 def add(obj, type):
     if type == "student":
-        sql = '''INSERT INTO student VALUES(%d, "%s", "%s", "%s", "%s", "%s", "%s");''' % (obj['sid'], obj['school'], obj['email'], obj['phone'], obj['fname'], obj['lname'], obj['password'])
+        sid = int(obj['sid'])
+        sql = '''INSERT INTO student VALUES(%d, "%s", "%s", "%s", "%s", "%s", "%s");''' % (sid, obj['school'], obj['email'], obj['phone'], obj['fname'], obj['lname'], obj['password'])
         query(sql)
 
-        sql = '''INSERT INTO student VALUES("%s", "%s")''' % (obj['sid'], obj['major'])
+        sql = '''INSERT INTO major VALUES(%d, "%s")''' % (sid, obj['major'])
         query(sql)
     elif type == "company":
-        sql = '''INSERT INTO company VALUES(%d, "%s", "%s", "%s", "%s");''' % (obj['cid'], obj['email'], obj['address'], obj['name'], obj['password'])
+        cid = int(obj['cid'])
+        sql = '''INSERT INTO company VALUES(%d, "%s", "%s", "%s", "%s");''' % (cid, obj['email'], obj['address'], obj['name'], obj['password'])
         query(sql)
     elif type == "work":
-        sql = '''INSERT INTO didworks VALUES(%d, "%s", "%s", "%s");''' % (obj["sid"], obj["name"], obj["description"], obj["category"])
+        sid = int(obj['sid'])
+        sql = '''INSERT INTO didworks VALUES(%d, "%s", "%s", "%s");''' % (sid, obj["name"], obj["description"], obj["category"])
         query(sql)
     elif type == "course":
-        sql = '''INSERT INTO course VALUES(%d, "%s", "%s", "%s");''' % (obj['code'], obj['name'], obj['credit'], obj['professor'])
-        query(sql)
+        sql = '''SELECT * FROM course WHERE code="%s"''' % obj['code']
+        result = query(sql)
+        if len(result) == 0:
+            sql = '''INSERT INTO course VALUES("%s", "%s", "%s", "%s");''' % (obj['code'], obj['name'], obj['credit'], obj['professor'])
+            query(sql)
 
-        sql = '''INSERT INTO takes VALUES(%d, "%s", "%s", "%s", "%s", %d)''' % (obj['sid'], obj['code'], obj['grade'], obj['year'], obj['semester'], obj['retake'])
+        sid = int(obj['sid'])
+        sql = '''INSERT INTO takes VALUES(%d, "%s", "%s", "%s", "%s")''' % (sid, obj['code'], obj['grade'], obj['year'], obj['semester'])
         query(sql)
     elif type == "position":
-        sql = '''INSERT INTO position VALUES("%s", %d, "%s", "%s");''' % (obj['pname'], obj['pid'], obj['description'], obj['required_GPA'])
+        pid = int(obj['pid'])
+        required_gpa = float(obj['required_gpa'])
+        sql = '''INSERT INTO position VALUES("%s", %d, "%s", %f);''' % (obj['name'], pid, obj['description'], required_gpa)
         query(sql)
 
-        sql = '''INSERT INTO holds VALUES(%d, %d);''' % (obj['cid'], obj['pid'])
+        cid = int(obj['cid'])
+        sql = '''INSERT INTO holds VALUES(%d, %d);''' % (cid, pid)
         query(sql)
+
+        sql = '''INSERT INTO requires VALUES("%s", %d);''' % (obj['code'], pid)
+        query(sql)
+
+def get(what, uid):
+    uid = int(uid)
+    if what == "position":
+        sql = '''SELECT position.pid, position.name, position.description, position.required_gpa 
+        FROM position, company, holds 
+        WHERE position.pid = holds.pid and holds.cid = company.cid and company.cid = %d;''' % uid
+        result = query(sql)
+        return result
+    elif what == "course":
+        sql = '''SELECT course.code, course.name, course.credit, takes.grade, course.professor, takes.year, takes.semester
+        FROM course, takes, student
+        WHERE course.code = takes.code and takes.sid = student.sid and student.sid = %d;''' % uid
+        result = query(sql)
+        return result
+    elif what == "work":
+        sql = '''SELECT didworks.name, didworks.description, didworks.category
+        FROM didworks, student
+        WHERE didworks.sid = student.sid and student.sid = %d;''' % uid
+        result = query(sql)
+        return result
+
+def calculate(what, uid):
+    uid = int(uid)
+    if what == "position":
+        courses = get("course", uid)
+        gpa = list(map(lambda course: float(course[2]), courses))
+        gpa = sum(gpa) / len(gpa)
+
+        sql = '''SELECT company.name, position.name, position.description 
+        FROM company, position, holds
+        WHERE position.pid = holds.pid and holds.cid = company.cid and position.required_gpa < %f;''' % gpa
+        result = query(sql)
+        return result
+
+def send(msg, f, t, sender):
+    # f - user id, t - user name
+    mid = int(random.random() * 10000000)
+    if sender == "student":
+        f = int(f)
+        sql = '''INSERT INTO message VALUES(%d, %d, (SELECT cid FROM company WHERE company.name = "%s"), now(), now(), "%s", "%s");''' % (mid, f, t, msg, sender)
+        query(sql)
+    else:
+        f = int(f)
+        t = int(t)
+        sql = '''INSERT INTO message VALUES(%d, %d, %d, now(), now(), "%s", "%s");''' % (mid, t, f, msg, sender)
+        query(sql)
+
+def receive(uid, receiver):
+    uid = int(uid)
+    if receiver == "student":
+        sql = '''SELECT message.text, company.name
+        FROM message, student, company
+        WHERE message.sid = student.sid and message.cid = company.cid and message.sender = "company" and student.sid = %d;''' % uid
+        result = query(sql)
+        return result
+    else:
+        sql = '''SELECT message.text, student.fname, student.lname, student.sid
+        FROM message, student, company
+        WHERE message.sid = student.sid and message.cid = company.cid and message.sender = "student" and company.cid = %d;''' % uid
+        result = query(sql)
+        return result
--- run.py
+++ run.py
@@ -3,14 +3,20 @@ import database as db
 
 app = Flask(__name__)
 
+#--------------------------------------------------------------------------------
+# Sign In
+#
+
+# Page - Sign In
 @app.route("/")
 @app.route("/index")
 @app.route("/index.html")
 def index():
     return render_template("index.html")
 
-@app.route("/signin", methods = ["POST"])
-def signin():
+# Process - Called when SignIn & SignUp
+@app.route("/_signin", methods = ["POST"])
+def _signin():
     if request.method == 'POST':
         uid = request.form["userid"]
         upw = request.form["userpw"]
@@ -24,111 +30,156 @@ def signin():
                 return redirect(url_for('company'))
 
         if stu == "student":    # as student
-            return redirect(url_for('studentmypage', Fname=result[4]))
+            return redirect(url_for('studentmypage', sid=result[0], Fname=result[4]))
         else:                   # as company
-            return redirect(url_for('companymypage', name=result[3]))
+            return redirect(url_for('companymypage', cid=result[0], name=result[3]))
 
-@app.route("/studentmypage/<Fname>/")
-def studentmypage(Fname):
-    return render_template("studentmypage.html", Fname = Fname)
+#--------------------------------------------------------------------------------
+# Sign In
+#
 
-@app.route("/companymypage/<name>/")
-def companymypage(name):
-    return render_template("companymypage.html", name = name)
+@app.route("/search")
+def search():
+    return render_template("search.html")
 
+#--------------------------------------------------------------------------------
+# Student
+#
+
+# Page - Student Enroll
 @app.route("/student")
 def student():
     return render_template("student.html")
 
+# Process - Student Enroll
+@app.route("/_studentenroll", methods=["POST"])
+def _studentenroll():
+    if request.method == "POST":
+        db.add(request.form, "student")
+
+        return redirect(url_for('studentmypage', sid = request.form["sid"], Fname = request.form["fname"]))
+    
+# Page - Student MyPage
+@app.route("/studentmypage/<sid>/<Fname>/")
+def studentmypage(sid, Fname):
+    course = db.get("course", sid)
+    work = db.get("work", sid)
+    position = db.calculate("position", sid)
+    message = db.receive(sid, "student")
+    return render_template("studentmypage.html", sid = sid, Fname = Fname, courses = course, works = work, positions = position, messages = message)
+
+# Page - Add Course
+@app.route("/studentmypage/<sid>/<Fname>/addcourse")
+def addcourse(sid, Fname):
+    return render_template("addcourse.html")
+
+# Process - Add Course
+@app.route("/studentmypage/<sid>/<Fname>/_addcourse", methods = ["POST"])
+def _addcourse(sid, Fname):
+    if request.method == "POST":
+        obj = {"sid": sid}
+        for field in ["code", "name", "credit", "professor", "grade", "year", "semester"]:
+            obj[field] = request.form[field]
+        db.add(obj, "course")
+
+        return redirect(url_for('studentmypage', sid = sid, Fname = Fname))
+
+# Page - Add Work
+@app.route("/studentmypage/<sid>/<Fname>/addwork")
+def addwork(sid, Fname):
+    return render_template("addwork.html")
+
+# Process - Add Work
+@app.route("/studentmypage/<sid>/<Fname>/_addwork", methods = ["POST"])
+def _addwork(sid, Fname):
+    if request.method == "POST":
+        obj = {"sid": sid}
+        for field in ["name", "description", "category"]:
+            obj[field] = request.form[field]
+        db.add(obj, "work")
+
+        return redirect(url_for('studentmypage', sid = sid, Fname = Fname))
+
+# Process - Send Message to Company
+@app.route("/studentmypage/<sid>/<Fname>/_sendtocompany", methods = ["POST"])
+def _sendtocompany(sid, Fname):
+    if request.method == "POST":
+        message = request.form['message']
+        name = request.form['name']
+        db.send(message, sid, name, "student")
+
+        return redirect(url_for("studentmypage", sid = sid, Fname = Fname))
+
+# Process - Send Message to Company
+@app.route("/studentmypage/<sid>/<Fname>/_sendtocompany/<name>", methods = ["POST"])
+def _sendtothecompany(sid, Fname, name):
+    if request.method == "POST":
+        message = request.form['message']
+        db.send(message, sid, name, "student")
+
+        return redirect(url_for("studentmypage", sid = sid, Fname = Fname))
+
+#--------------------------------------------------------------------------------
+# Company
+#
+
+# Page - Company Enroll
 @app.route("/company")
 def company():
     return render_template("company.html")
 
+# Process - Company Enroll
+@app.route("/_companyenroll", methods=["POST"])
+def _companyenroll():
+    if request.method == "POST":
+        db.add(request.form, "company")
+
+        return redirect(url_for('companymypage', name = request.form["name"], cid = request.form["cid"]))
+    
+# Page - Company MyPage
+@app.route("/companymypage/<name>/<cid>/")
+def companymypage(name, cid):
+    position = db.get("position", cid)
+    message = db.receive(cid, "company")
+    return render_template("companymypage.html", name = name, positions = position, messages = message)
+
+# Page - Add Position
+@app.route("/companymypage/<name>/<cid>/addposition")
+def addposition(name, cid):
+    return render_template("addposition.html")
+
+# Process - Add Position
+@app.route("/companymypage/<name>/<cid>/_addposition", methods = ["POST"])
+def _addposition(name, cid):
+    if request.method == "POST":
+        obj = {"cid": cid}
+        for field in ["name", "pid", "description", "required_gpa", "code"]:
+            obj[field] = request.form[field]
+        db.add(obj, "position")
+
+        return redirect(url_for('companymypage', name = name, cid = cid))
+
+# Process - Send Message to Student
+@app.route("/companymypage/<name>/<cid>/_sendtostudent", methods = ["POST"])
+def _sendtostudent(name, cid):
+    if request.method == "POST":
+        message = request.form['message']
+        sid = request.form['sid']
+        db.send(message, cid, sid, "company")
+
+        return redirect(url_for('companymypage', name = name, cid = cid))
+
+# Process - Send Message to Student
+@app.route("/companymypage/<name>/<cid>/_sendtostudent/<sid>", methods = ["POST"])
+def _sendtothestudent(name, cid, sid):
+    if request.method == "POST":
+        message = request.form['message']
+        db.send(message, cid, sid, "company")
+
+        return redirect(url_for('companymypage', name = name, cid = cid))
+
+#--------------------------------------------------------------------------------
+# run
+#
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000)
-
-
-'''
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-# url for select
-@app.route("/select")
-def select():
-    # Connect to database
-    db = pymysql.connect(host='localhost',
-                         port=3306,
-                         user='root',
-                         passwd='1111',
-                         db='cs360',
-                         charset='utf8')
-    try:
-        # Set cursor to the database
-        with db.cursor() as cursor:
-            # Write SQL query
-            sql = "SELECT * FROM EMPLOYEE;"
-            # Execute SQL
-            cursor.execute(sql)
-
-            # Fetch the result
-            # result is dictionary type
-            result = cursor.fetchall()
-            # Print tuples
-
-            output = ""
-            for row in result:
-                output += "{0} {1}\n".format(row[0], row[2])
-    finally:
-        db.close()
-
-    return render_template('select.html', result=output)
-
-@app.route("/redirect_insert")
-def redirect_insert():
-    return render_template('insert.html')
-
-@app.route("/insert", methods = ['POST'])
-def insert():
-    if request.method == 'POST':
-        fname = request.form['FirstName']
-        lname = request.form['LastName']
-        ssn = request.form['Ssn']
-        return redirect(url_for('insert_sent', Fname = fname, Lname = lname, Ssn = ssn))
-
-@app.route("/insert_sent/<Fname>/<Lname>/<Ssn>")
-def insert_sent(Fname, Lname, Ssn):
-    # Connect to database
-    db = pymysql.connect(host='localhost',
-                         port=3306,
-                         user='root',
-                         passwd='1111',
-                         db='cs360',
-                         charset='utf8')
-    try:
-        # Set cursor to the database
-        with db.cursor() as cursor:
-            # Write SQL query
-            sql = """INSERT INTO EMPLOYEE
-                     VALUES('""" + Fname + """',
-                            'E','"""\
-                            + Lname + "','"\
-                            + Ssn + """',
-                            '1937-11-10',
-                            '450 Stone, Houston, TX',
-                            'M',
-                            55000,
-                            NULL,
-                            1);"""
-            # Execute SQL
-            cursor.execute(sql)
-        # You must manually commit after every DML methods.
-        db.commit()
-    finally:
-        db.close()
-
-    return redirect("/")
-
-if __name__ == "__main__":
-    app.run('0.0.0.0', port=5000)
-'''
